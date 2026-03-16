6b0ab68a4e4db950d3676124337e558038c2284c
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2026-03-16 13:40:11 -0500

    main calculator frame inplemented

--- src/ui/CalculatorFrame.java
+++ src/ui/CalculatorFrame.java
@@ -1,5 +1,41 @@
 package ui;
 
-public class CalculatorFrame {
+import javax.swing.*;
+import java.awt.*;
 
+public class CalculatorFrame extends JFrame {
+    private TopPanel topPanel;
+    private HistoryPanel historyPanel;
+    private DisplayPanel displayPanel;
+    private ButtonPanel buttonPanel;
+    
+    public CalculatorFrame(){
+        initializeFrame();
+        initializePanels();
+        layoutPanels();
+
+        setVisible(true);
+    }
+
+    private void initializeFrame(){
+        setTitle("Calculator");
+        setSize(410, 700);
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    private void initializePanels(){
+        topPanel = new TopPanel();
+        historyPanel = new HistoryPanel();
+        displayPanel = new DisplayPanel();
+        buttonPanel = new ButtonPanel();
+    }
+
+    private void layoutPanels(){
+        add(topPanel, BorderLayout.NORTH);
+        add(historyPanel, BorderLayout.WEST);
+        add(displayPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
 }
